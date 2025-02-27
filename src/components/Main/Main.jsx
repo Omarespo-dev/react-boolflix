ad90c7ec5887ec9897296a4cdf59edc47db23c6a
Omarespo-dev <omaresposito21@gmail.com>
2025-02-27 20:03:52 +0100

    Added milestone 3 star

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,6 +4,13 @@ import { useContext } from "react";
 // Importo Flag
 import Flag from 'react-world-flags';
 
+// FONT AWESOME
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+
+
+
+
 const languageToCountry = {
     en: "GB",
     it: "IT",
@@ -12,6 +19,7 @@ const languageToCountry = {
     de: "DE",
 
 };
+
 
 // Importo GlobalContext
 import GlobalContex from "./../../contexts/GlobalContext"
@@ -25,26 +33,62 @@ export default function Main() {
             <main>
                 <h1>FILM</h1>
                 {films.map(film => {
-                    const countryCode = languageToCountry[film.original_language] || "ZW";  // Qui definisci la variabile
+                    
+                    const countryCode = languageToCountry[film.original_language] || "ZW";  
+
+                    const votoFilm =Math.ceil(film.vote_average / 2)
+                    // console.log(votoFilm)
+
+                    // Genero stelle utilizzo ciclo for
+                    let stelle = []
+                    for (let i = 0; i < 5; i++) {
+                        stelle.push(
+                            <FontAwesomeIcon 
+                            key={i}
+                            icon={faStar} 
+                            style={{color: i < votoFilm ? "#ffff80" : "#c0c0c0"}} />
+                        )
+
+                    }
 
                     return (
-                        <section className="set-section" key={film.id}>
+                        <section className="set-section"key={film.id}>
                             <h2>{film.title}</h2>
                             <h3>{film.original_title}</h3>
                             <img src={`https://image.tmdb.org/t/p/w342${film.backdrop_path}`} alt={`${film.original_title}`} />
                             <p>{film.original_language}
                                 <Flag code={countryCode} style={{ width: 50, height: 30 }} />
                             </p>
-                            <p>{film.vote_average}</p>
+                            <p>{votoFilm.toFixed(0)}</p>
+                            <div>
+                                <p>
+                                    {stelle}
+                                </p>
+                            </div>
                         </section>
+
                     );
                 })}
 
 
                 <h1>SERIE TV</h1>
                 {series.map(serie => {
-                    const countryCode = languageToCountry[serie.original_language] || "ZW";  // Qui definisci la variabile
+                    const countryCode = languageToCountry[serie.original_language] || "ZW";  
+                    const votoSerie=Math.ceil(serie.vote_average / 2)
+                    // console.log(votoSerie)
 
+                    // Genero stelle utilizzo ciclo for
+                    let stelle = []
+                    for (let i = 0; i < 5; i++) {
+                        stelle.push(
+                            <FontAwesomeIcon 
+                            key={i}
+                            icon={faStar} 
+                            style={{color: i < votoSerie ? "#ffff80" : "#c0c0c0"}} />
+                        )
+
+                    }
+                    
                     return (
                         <section className="set-section" key={serie.id}>
                             <h2>{serie.name}</h2>
@@ -53,7 +97,12 @@ export default function Main() {
                             <p>{serie.original_language}
                                 <Flag code={countryCode} style={{ width: 50, height: 30 }} />
                             </p>
-                            <p>{serie.vote_average}</p>
+                            <p>{votoSerie.toFixed(0)}</p>
+                            <div>
+                                <p>
+                                    {stelle}
+                                </p>
+                            </div>
                         </section>
                     );
                 })}
